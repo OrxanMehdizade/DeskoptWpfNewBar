c2e682ad688aa78e18adf2f824191d94ad1ccda3
Mehdi_ha01 <Mehdi_ha01@STHQ0124-14>
2023-03-28 20:19:34 +0400

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void growing_Click_1(object sender, RoutedEventArgs e)
+        {
+            Bar.Value += 5;
+        }
+
+        private void descending_Click_1(object sender, RoutedEventArgs e)
+        {
+            Bar.Value -= 5;
+        }
+
+
+    }
+}
